27f8b9a68a70a6ce44938f06c81719076cd9e2ca
xianlin-zhao <zhaoxianlin@pku.edu.cn>
2022-03-02 16:23:15 +0800

    removelast

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -82,7 +82,7 @@ public class ArrayDeque<T> {
 //        items[newid] = null;
         nextLast = newid;
         size -= 1;
-        if (size >= 16 && size / items.length < 0.25) {
+        if (size >= 16 && (double)size / items.length < 0.25) {
             resize((int) (items.length / 2));
         }
         return ans;
