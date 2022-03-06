4eb63284cc3776d81f362a53591a272b6de593da
xianlin-zhao <zhaoxianlin@pku.edu.cn>
2022-03-06 20:12:42 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,86 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int N;
+    private int[] grids;
+    private int openNum;
+    private WeightedQuickUnionUF wqu;
 
+    public Percolation(int N) {
+       if (N <= 0) {
+           throw new IllegalArgumentException();
+       }
+       this.N = N;
+       openNum = 0;
+       grids = new int[N * N + 2];
+       wqu = new WeightedQuickUnionUF(N * N + 2);
+       for (int i = 0; i < N * N; i++) {
+           grids[i] = -1;
+       }
+    }
+
+    private int twoToNo(int row, int col) {
+        return row * N + col;
+    }
+
+    public void open(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IllegalArgumentException();
+        };
+        int id = twoToNo(row, col);
+        if (row == 0) {
+            wqu.union(N * N, id);
+        }
+        if (row == N - 1) {
+            wqu.union(N * N + 1, id);
+        }
+        openNum++;
+        grids[id] = id;
+
+        int[][] direction = {
+                {-1, 0},
+                {1, 0},
+                {0, -1},
+                {0, 1}
+        };
+        for (int i = 0; i < 4; i++) {
+            int newX = row + direction[i][0];
+            int newY = col + direction[i][1];
+            if (newX < 0 || newX >= N || newY < 0 || newY >= N) {
+                continue;
+            }
+            int newId = twoToNo(newX, newY);
+            if (isOpen(newX, newY)) {
+                wqu.union(id, newId);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IllegalArgumentException();
+        };
+        int id = twoToNo(row, col);
+        return grids[id] >= 0;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IllegalArgumentException();
+        };
+        int id = twoToNo(row, col);
+        return wqu.connected(N * N, id);
+    }
+
+    public int numberOfOpenSites() {
+        return openNum;
+    }
+
+    public boolean percolates() {
+        return wqu.connected(N * N, N * N + 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,48 @@
 package hw2;
 
 public class PercolationStats {
+    private int N;
+    private int T;
+    private double mu;
+    private double dev;
+    private double thresholds[];
 
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        this.T = T;
+        thresholds = new double[T];
+        for (int i = 0; i < T; i++) {
+            int num = 0;
+            Percolation per = pf.make(N);
+            while (!per.percolates()) {
+                int randX = edu.princeton.cs.introcs.StdRandom.uniform(0, N);
+                int randY = edu.princeton.cs.introcs.StdRandom.uniform(0, N);
+                while (per.isOpen(randX, randY)) {
+                    randX = edu.princeton.cs.introcs.StdRandom.uniform(0, N);
+                    randY = edu.princeton.cs.introcs.StdRandom.uniform(0, N);
+                }
+                per.open(randX, randY);
+                num++;
+            }
+            thresholds[i] = (double) num / (N * N);
+        }
+        mu = edu.princeton.cs.introcs.StdStats.mean(thresholds);
+        dev = edu.princeton.cs.introcs.StdStats.stddev(thresholds);
+    }
+
+    public double mean() {
+        return mu;
+    }
+
+    public double stddev() {
+        return dev;
+    }
+
+    public double confidenceLow() {
+        return mu - (1.96 * dev / Math.sqrt(T));
+    }
+
+    public double confidenceHigh() {
+        return mu + (1.96 * dev / Math.sqrt(T));
+    }
 }
