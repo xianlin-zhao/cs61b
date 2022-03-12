401704c345fef9a9cf3368ddb7aab03ccc2f585d
xianlin-zhao <zhaoxianlin@pku.edu.cn>
2022-03-12 13:01:24 +0800

    finish project3 phase1

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -13,6 +13,33 @@ public class Rasterer {
         // YOUR CODE HERE
     }
 
+    private boolean outOfBound(double ullon, double ullat, double lrlon, double lrlat) {
+        if (ullon > lrlon || ullat < lrlat) {
+            return true;
+        }
+        if (lrlon <= MapServer.ROOT_ULLON || lrlat >= MapServer.ROOT_ULLAT) {
+            return true;
+        }
+        if (ullon >= MapServer.ROOT_LRLON || ullat <= MapServer.ROOT_LRLAT) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getDepth(double ullon, double lrlon, double w) {
+        double wish = (lrlon - ullon) / w;
+        double ori = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / MapServer.TILE_SIZE;
+        int depth = 0;
+        while (depth < 7) {
+            if (ori <= wish) {
+                break;
+            }
+            ori = ori / 2.0;
+            depth++;
+        }
+        return depth;
+    }
+
     /**
      * Takes a user query and finds the grid of images that best matches the query. These
      * images will be combined into one big image (rastered) by the front end. <br>
@@ -44,8 +71,70 @@ public class Rasterer {
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        double ullon = params.get("ullon");
+        double lrlon = params.get("lrlon");
+        double ullat = params.get("ullat");
+        double lrlat = params.get("lrlat");
+        double w = params.get("w");
+        double h = params.get("h");
+
+        String[][] render_grid;
+        double raster_ul_lon = 0.0;
+        double raster_ul_lat = 0.0;
+        double raster_lr_lon = 0.0;
+        double raster_lr_lat = 0.0;
+        int depth = 0;
+        boolean query_success = true;
+
+        if (outOfBound(ullon, ullat, lrlon, lrlat)) {
+            query_success = false;
+            render_grid = new String[1][1];
+        } else {
+            depth = getDepth(ullon, lrlon, w);
+            double widthPerPic = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / Math.pow(2.0, depth);
+            double heightPerPic = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / Math.pow(2.0, depth);
+            int xL = (int) Math.floor((ullon - MapServer.ROOT_ULLON) / widthPerPic);
+            int xR = (int) Math.floor((lrlon - MapServer.ROOT_ULLON) / widthPerPic);
+            int yL = (int) Math.floor((MapServer.ROOT_ULLAT - ullat) / heightPerPic);
+            int yR = (int) Math.floor((MapServer.ROOT_ULLAT - lrlat) / heightPerPic);
+
+            raster_ul_lon = MapServer.ROOT_ULLON + xL * widthPerPic;
+            raster_lr_lon = MapServer.ROOT_ULLON + (xR + 1) * widthPerPic;
+            raster_ul_lat = MapServer.ROOT_ULLAT - yL * heightPerPic;
+            raster_lr_lat = MapServer.ROOT_ULLAT - (yR + 1) * heightPerPic;
+
+            if (ullon < MapServer.ROOT_ULLON) {
+                xL = 0;
+                raster_ul_lat = MapServer.ROOT_ULLON;
+            }
+            if (ullat > MapServer.ROOT_ULLAT) {
+                yL = 0;
+                raster_ul_lat = MapServer.ROOT_ULLAT;
+            }
+            if (lrlon > MapServer.ROOT_LRLON) {
+                xR = (int) Math.pow(2.0, depth) - 1;
+                raster_lr_lon = MapServer.ROOT_LRLON;
+            }
+            if (lrlat < MapServer.ROOT_LRLAT) {
+                yR = (int) Math.pow(2.0, depth) - 1;
+                raster_lr_lat = MapServer.ROOT_LRLAT;
+            }
+
+            render_grid = new String[yR - yL + 1][xR - xL + 1];
+            for (int i = yL; i <= yR; i++) {
+                for (int j = xL; j <= xR; j++) {
+                    render_grid[i - yL][j - xL] = "d" + depth + "_x" + j + "_y" + i + ".png";
+                }
+            }
+        }
+
+        results.put("render_grid", render_grid);
+        results.put("raster_ul_lon", raster_ul_lon);
+        results.put("raster_ul_lat", raster_ul_lat);
+        results.put("raster_lr_lon", raster_lr_lon);
+        results.put("raster_lr_lat", raster_lr_lat);
+        results.put("depth", depth);
+        results.put("query_success", query_success);
         return results;
     }
 
